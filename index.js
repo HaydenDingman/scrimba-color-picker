7ea104d724d128237a5292509a5cccbe862004a0
Hayden Dingman <haydendingman@gmail.com>
2025-05-30 18:57:03 -0700

    Refactor API call with createColorSwatch function

--- index.js
+++ index.js
@@ -6,17 +6,21 @@ fetch("https://www.thecolorapi.com/scheme?hex=0047AB&mode=analogic&count=5")
         const colors = data.colors;
         colorSwatchContainer.innerHTML = "";
         colors.forEach((color) => {
-            const newSwatch = document.createElement("div");
-            newSwatch.classList.add("swatch");
-            const newSwatchColor = document.createElement("div");
-            newSwatchColor.classList.add("swatch-color");
-            newSwatchColor.style.backgroundColor = color.hex.value;
-            const newFooter = document.createElement("div");
-            newFooter.textContent = color.hex.value;
-
-            newSwatch.append(newSwatchColor, newFooter);
-            colorSwatchContainer.append(newSwatch);
+            createColorSwatch(color);
         })
-
-        colorSwatchContainer.innerHTML = colorHtml;
     });
+
+
+function createColorSwatch(color) {
+    const newSwatch = document.createElement("div");
+    newSwatch.classList.add("swatch");
+    const newSwatchColor = document.createElement("div");
+    newSwatchColor.classList.add("swatch-color");
+    newSwatchColor.style.backgroundColor = color.hex.value;
+    const newFooter = document.createElement("div");
+    newFooter.classList.add("footer");
+    newFooter.textContent = color.hex.value;
+
+    newSwatch.append(newSwatchColor, newFooter);
+    colorSwatchContainer.append(newSwatch);
+}
